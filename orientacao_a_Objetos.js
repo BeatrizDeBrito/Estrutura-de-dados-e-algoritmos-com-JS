e72f1a71a98b9d423851c98583498a5885d8bb01
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2023-10-26 17:31:00 -0300

    POO em JavaScript

---
+++ orientacao_a_Objetos.js
@@ -0,0 +1,13 @@
+function Book(title, pages, isbn){
+    this.title = title;
+    this.pages = pages;;
+    this.isbn = isbn;
+    this.printIsbn = function(){
+        console.log(this.isbn) // método diretamente na definição da classe
+    }
+}
+
+//instancia
+var book = new Book('A Revolução dos Bichos', '200', '1020');
+book.printIsbn(); // método
+console.log(book.title)
