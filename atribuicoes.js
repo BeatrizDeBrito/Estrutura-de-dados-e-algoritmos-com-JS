ae9db0d1477665bd238688b69ae8858a4d59d107
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2023-10-27 16:25:58 -0300

    Uso do const

---
+++ atribuicoes.js
@@ -0,0 +1,13 @@
+const jsFramework =  {
+    name: 'Angular'
+}
+
+jsFramework.name = 'React' // permite atribuição por ser um objeto
+
+console.log(jsFramework)
+
+// jsFramework = {
+//     name: 'Vue'
+// }
+
+// console.log(jsFramework) dá um erro, pois não é possível atribuir outra referência à essa variável
