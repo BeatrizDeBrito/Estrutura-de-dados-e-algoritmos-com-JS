238f263c5874f05909f4ab6efddfb78c9cb98782
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2023-10-30 17:42:56 -0300

    Getters e setters

---
+++ getters_e_setters.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(name){
+        this._name = name;
+    }
+    get name(){
+        return  this._name;
+    }
+    set name(value){
+        this._name = value
+    }
+}
+let lotrChar = new Person('Frodo')
+console.log(lotrChar.name)
