5bdc82cf9d221a7d8bd8dad966fa5ace2cfa6832
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2023-11-08 15:21:27 -0300

    Modifica elementos do array

--- JS/acrescentando_elementos_array.js
+++ JS/acrescentando_elementos_array.js
@@ -7,3 +7,15 @@ console.log(numbers)
 
 numbers.pop(10)
 console.log(numbers)
+
+numbers.unshift(200) //adiciona no primeiro elemento
+console.log(numbers)
+
+numbers.shift(200) //remove no primeiro elemento
+console.log(numbers)
+
+numbers.unshift(200)
+console.log(numbers)
+
+numbers.splice(0,5) //a partir do índice 0, remove 5 elementos
+console.log(numbers)
